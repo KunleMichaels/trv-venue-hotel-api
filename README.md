af30b5a60278dce0428ece7adfa74bd26ceade48
Michael Kolawole <mkolawole@coursera.org>
2021-04-18 07:45:35 -0700

    data schema

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TRV VENUE HOTEL API
+
+Paginated API
+Sort and Filter on the server
+    this scales better with larger dataset 
+    server-side filtering will always operate on up-to-date dataset
+    user would not need to download all available data initially (this also defeats the essence of pagination)
+Route based request
---
+++ schema.ts
@@ -0,0 +1,28 @@
+
+export type User = {
+
+}
+
+export type Room = {
+    id: string;
+    name: string;
+    description: string;
+    max_occupancy: 1 | 2 | 3 | 4;
+    rating: 1 | 2 | 3 | 4 | 5;
+    price_in_usd: number;
+    available: boolean;
+}
+
+export type Amenity = 'free_parking' | 'free_wifi' | 'gym' | 'pets' | 'pool' | 'restaurant' | 'spa';
+
+export type Hotel = {
+    id: string;
+    name: string;
+    description: string;
+    distance_to_venue: number;
+    rating: 1 | 2 | 3 | 4 | 5;
+    price_category: 'low' | 'medium' | 'high';
+    amenities: [Amenity?, Amenity?, Amenity?, Amenity?, Amenity?]
+    images: string[];
+    rooms: Room[];
+}
